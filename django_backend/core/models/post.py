011b454d8c0dc86b03498423f036e7358cdcec65
Your Name <your@email.com>
2021-02-04 18:13:31 +0100

    Added PostSerializer and PostView

--- django_backend/core/models/post.py
+++ django_backend/core/models/post.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Post (models):
+class Post(models.Model):
     title = models.CharField(max_length=250)
     content = models.TextField()
 
--- django_backend/core/views.py
+++ django_backend/core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import PostSerializer
+from .models import Post
 
 # Create your views here.
+
+class PostView(generics.RetrieveAPIView):
+    queryset = Posts.objects.all()
+
+    def get(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = PostSerializer(queryset, manu=True)
+        return Response(serializer.data)
